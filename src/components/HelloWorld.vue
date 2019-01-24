31b11e02d158435122bfab4d485cb1b2935919c2
Andre Lorch <andre.lorch@kuehne-nagel.com>
2019-01-24 15:59:47 +0100

    WIP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <span>
-    {{ timestamp }}
-    </span>
+    <span>{{ locationData }}</span>
     <button @click="unixTime()">Show me the time</button>
     <button @click="timestampToHour()">Show me the hours</button>
-    </div>
+    <span>This is formatted: {{ dst }}</span>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -14,27 +13,82 @@ export default {
   // el: "#app",
   data() {
     return {
+      locationData: null,
       info: null,
       timestamp: null,
-      hours: null
+      hours: null,
+      dst: null,
+      formatted: null
     };
   },
+  watch: {
+    locationData: function() {
+      this.timestamp = this.locationData.timestamp;
+
+      /**
+       * I need to update all these paremeter using this watch.
+       * I'll need to write computed (?) properties to deal with this.
+       */
+    },
+    dst: function() {
+      this.dst = this.locationData.dst;
+    },
+    formatted: function() {
+      this.formatted = this.locationData.formatted;
+    }
+  },
+  // watch: {
+  //   locationTimestamp() {
+  //     this.timestamp = this.locationData.zones[0].timestamp;
+  //   }
+  // },
+  // computed: {
+  //   locationTimestamp() {
+  //     this.timestamp = this.locationData.zones[0].timestamp;
+  //     // return this.timestamp;
+  //   }
+  // },
   mounted() {
     axios
-      .get("http://api.timezonedb.com/v2.1/list-time-zone?key=PVVEZXX7L9E5&format=json&country=NZ")
+      .get(
+        "http://api.timezonedb.com/v2.1/get-time-zone?key=PVVEZXX7L9E5&format=json&by=zone&zone=Europe/Berlin"
+      )
       // .then(response => (console.log(response.data.zones[0].timestamp)))
       // .then(response => (this.data = response.data))
-      .then(response => (this.timestamp = response.data.zones[0].timestamp))
+      .then(response => (this.locationData = response.data));
   },
   methods: {
-    unixTime: function (timestamp) {
+    unixTime() {
       console.log(this.timestamp);
+      console.log(this.info);
     },
-    timestampToHour: function (timestamp) {
-       const timeToHour = function () {
-        (this.hours * 1000).getHours();
-      }
-      console.log(timeToHour);
+    date() {
+      const thisTimeStamp = new Date(this.timestamp * 1000);
+      return thisTimeStamp;
+    },
+    currentHours() {
+      return this.date().getHours();
+    },
+    currentMinutes() {
+      return "0" + this.date().getMinutes();
+    },
+    currentSeconds() {
+      return "0" + this.date().getSeconds();
+    },
+    timestampToHour() {
+      const formattedTime = `${this.currentHours()}:${this.currentMinutes().substr(
+        -2
+      )}:${this.currentSeconds().substr(-2)}`;
+      //  const timeToHour = function () {
+      // (this.hours * 1000).getHours();
+      // }
+      // console.log(() => (this.timestamp * 1000).getHours());
+
+      // console.log((this.currentHours()));
+      // console.log((this.currentMinutes()));
+      // console.log((this.currentSeconds()));
+      console.log(formattedTime);
+      // return (this.timestamp * 1000).getHours();
     }
   }
 };
