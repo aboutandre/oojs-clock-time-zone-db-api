5f947c9aab94d2e847c8a77f25e3cd4115aceeb5
André Oliveira <github@aboutandre.com>
2019-01-28 17:32:36 +0100

    ADD select component to populate with timezones

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld />
+    <v-select :options="['foo','bar']"></v-select>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import vSelect from '../node_modules/vue-select/src/components/Select.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    vSelect
   }
 }
 </script>
---
+++ src/components/TimeZoneSelector.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+
+  },
+  watch: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
